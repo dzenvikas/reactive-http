139f1dd24b6133786cee0dbb07c82d625d44111d
dzenvikas <vikaskumar.vsk@gmail.com>
2019-04-29 00:33:06 +0530

    Using history prop from Route to navigate to FullPost from a Post

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import axiosInstance from '../../../axiosInstance';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-import { Link } from 'react-router-dom';
 
 class Posts extends Component {
   state = {
-    posts: [],
-    selectedPostId: null
+    posts: []
   };
   componentDidMount() {
     axiosInstance.get('posts/').then(response => {
@@ -24,18 +22,16 @@ class Posts extends Component {
   }
 
   postSelectedHandler = id => {
-    this.setState({ selectedPostId: id });
+    this.props.history.push({ pathname: '/' + id });
   };
   render() {
     const posts = this.state.posts.map(post => {
       return (
-        <Link to={'/' + post.id} key={post.id}>
-          <Post
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        </Link>
+        <Post
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
       );
     });
     return <section className="posts">{posts}</section>;
